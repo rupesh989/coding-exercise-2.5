ace124cd3bda7b62003607607b8edb21988976cd
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2026-01-15 18:06:39 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class LogEntry(BaseModel):
-    id: int
-    timestamp: datetime
-    level: str
-    component: str
-    message: str
